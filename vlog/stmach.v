8f48136296f21f715889b8c1f87748b29fdc0336
Azman M. Yusof <azman@my1matrix.net>
2016-06-05 22:50:28 +0800

    Yeah... not needed.
    - work in progress for stmach

--- vlog/stmach.v
+++ vlog/stmach.v
@@ -1,51 +1,93 @@
-module stmach (clock, reset, codein, status);
-input clock,reset;
-input[7:0] codein; // code instructions
-output[2:0] status; // machine cycle types (output pin!)
-reg[2:0] status; // regged to maintain value through cycles
+module stmach (clk, rst, code, ipin, opin);
+
+parameter DATASIZE=8;
+parameter STATE_TR=4'b0000; // reset state
+parameter STATE_T1=4'b0001; // state 1
+parameter STATE_T2=4'b0010; // state 2
+parameter STATE_T3=4'b0011; // state 3
+parameter STATE_T4=4'b0100; // state 4
+parameter STATE_T5=4'b0101; // state 5
+parameter STATE_T6=4'b0110; // state 6
+parameter STATE_TH=4'b0111; // hold state
+parameter STATE_TW=4'b1000; // wait state
+parameter STATE_TT=4'b1001; // halt state
+parameter STAT_OF=3'b000; // opcode fetch cycle
+parameter IPIN_READY=0;
+parameter IPIN_HOLD=1;
+parameter IPIN_COUNT=2;
+parameter OPIN_S0=0;
+parameter OPIN_S1=1;
+parameter OPIN_IOM_=2;
+parameter OPIN_COUNT=3;
+
+input clk,rst;
+input[DATASIZE-1:0] code; // code from Instruction Register
+input[IPIN_COUNT-1:0] ipin;
+output[OPIN_COUNT-1:0] opin;
+wire[OPIN_COUNT-1:0] opin;
 
 // internal registers & nodes
-reg[3:0] state; // 4-bit encoded states = 6+4 used states
-// 0 - RESET State, 1:6 - State 1 to State 6
-// 7 - HOLD State, 9 - HALT State, 10 - WAIT State
+reg[3:0] cstate; // 4-bit encoded states = 6+4 used states
+reg[2:0] stat;
 reg bimc, halt, lmc, fmc, go6;
-wire[15:0] decstate; // decoded states - active high
-wire[8:0] tmpstate; // tmp nxtstate values (T10 to T0,T9 to T8)
-wire[3:0] nxstate; // encoded calculated next state values
+reg[3:0] nstate; // encoded calculated next state values
+
+// direct reg to pin
+assign opin[2:0] = stat;
 
 // state machine trigger
-
-always @(posedge clock or reset)
+always @(posedge clk or posedge rst)  // asynchronous reset!?
 begin
-	if(reset == 1) // actually active low
-	begin
-		state <= 4'b0; // goto state T0
-		// FIND!: how many clocks in RESET states??
-		status <= 0; // opcode fetch cycle
-		outpin <= 0; // actually active low? - invert!
+	if(rst == 1) begin // actually active low
+		cstate <= STATE_TR;
+		// internal register & node updates??
+		stat <= STAT_OF;
 		bimc <= 0; // not bus idle cycle
 		halt <= 0; // no halt
 		lmc <= 0; // not last cycle
 		fmc <= 1; // is first cycle
 		go6 <= 0; // not 6-state cycle
 	end
-	else
-	begin
-		state <= nxstate;
-		// internal register & node updates??
-		//status <= 0; // opcode fetch cycle
-		//outpin <= 0; // actually active low? - invert!
-		//bimc <= 0; // not bus idle cycle
-		halt <= 0; // no halt
-		lmc <= 0; // not last cycle
-		fmc <= 1; // is first cycle
-		go6 <= 0; // not 6-state cycle
+	else begin
+		cstate <= nstate;
 	end
 end
 
 // state selection logic
+always @(cstate or ipin of stat or bimc or halt or lmc or fmc or go6)
+begin
+	case (cstate)
+		STATE_TR: begin
+			// FIND!: how many clocks in RESET states??
+			nstate <= STATE_T1;
+		end
+		STATE_T1: begin
+			if (halt) begin
+				nstate <= STATE_TH;
+			end
+			else begin
+				nstate <= STATE_T2;
+			end
+		end
+		STATE_T2: begin
+			if (~(ipin[IPIN_READY]|bimc)) begin
+				nstate <= STATE_TW;
+			end
+			else begin
+				nstate <= STATE_T3;
+			end
+		end
+		STATE_T3: begin
+			nstate <= STATE_T3;
+		end
+		STATE_TW: begin
+			if (ipin[IPIN_READY]|bimc) begin
+				nstate <= STATE_T1;
+			end
+		end
+	endcase
+end
 
-assign tmpstate[0] = decstate[2] & ~(inpin[0] | bimc); // wait state
 assign tmpstate[1] = decstate[0] | (((decstate[3] & ~fmc) |
 	(decstate[4] & ~go6) | decstate[6]) & (~lmc | ~outpin[0]));
 assign tmpstate[2] = decstate[1];
@@ -56,7 +98,6 @@ assign tmpstate[5] = decstate[4] & go6;
 assign tmpstate[6] = decstate[5];
 assign tmpstate[7] = ((decstate[3] & ~fmc) | (decstate[4] & ~go6) |
 	decstate[6]) & lmc & outpin[0] ; // hold state
-assign tmpstate[8] = decstate[1] & halt; // halt state
 
 assign nxstate[0] = tmpstate[1] | tmpstate[3] | tmpstate[5] |
 	tmpstate[7] | tmpstate[8];
@@ -66,7 +107,5 @@ assign nxstate[2] = tmpstate[4] | tmpstate[5] | tmpstate[6] | tmpstate[7];
 assign nxstate[3] = tmpstate[8] | tmpstate[0];
 
 // output assignment
-
-decode4b decst (state,decstate); // not all output used
 
 endmodule
