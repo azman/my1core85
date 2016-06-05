d7633a3c861814f51e057b305a139efc4763b837
Azman M. Yusof <azman@my1matrix.net>
2016-06-05 09:30:05 +0800

    Added decoder module (with testbench)
    - fully customizable (just specify input size, default 3)
    - using multiple input gate (& unary operator)

---
+++ hdl/decoder.v
@@ -0,0 +1,45 @@
+module decoder ( iS, oY );
+
+parameter SEL_SIZE=3;
+parameter OUT_SIZE=2**SEL_SIZE;
+parameter CHK_SIZE=OUT_SIZE*SEL_SIZE;
+parameter ONE_COLD=0;
+
+input[SEL_SIZE-1:0] iS;
+output[OUT_SIZE-1:0] oY;
+
+wire[OUT_SIZE-1:0] oY;
+wire[CHK_SIZE-1:0] tS;
+
+genvar index,check,istep;
+
+generate
+for (check=0;check<SEL_SIZE;check=check+1) begin
+	localparam count = 2**check;
+	localparam steps = 2**(check+1);
+	// for inverts
+	for (index=0;index<OUT_SIZE;index=index+steps) begin
+		for (istep=0;istep<count;istep=istep+1) begin
+			assign tS[((index+istep)*SEL_SIZE)+check] = ~iS[check];
+		end
+	end
+	// for directs
+	for (index=0;index<OUT_SIZE;index=index+steps) begin
+		for (istep=0;istep<count;istep=istep+1) begin
+			assign tS[((index+istep+count)*SEL_SIZE)+check] = iS[check];
+		end
+	end
+end
+endgenerate
+
+generate
+for (index=0;index<OUT_SIZE;index=index+1)
+begin
+	if (ONE_COLD)
+		assign oY[index] = ~&tS[index*SEL_SIZE+(SEL_SIZE-1):index*SEL_SIZE];
+	else
+		assign oY[index] = &tS[index*SEL_SIZE+(SEL_SIZE-1):index*SEL_SIZE];
+end
+endgenerate
+
+endmodule
---
+++ hdl/decoder_tb.v
@@ -0,0 +1,50 @@
+module decoder_tb ();
+
+parameter MYSIZE = 3;
+parameter OPSIZE=2**MYSIZE;
+
+reg[MYSIZE-1:0] iS;
+wire[OPSIZE-1:0] oY, oX;
+reg[OPSIZE-1:0] oZ; // for checking
+
+integer loop;
+initial
+begin
+	// test 1-hot
+	oZ = 1; // only LSB 1, the rest should be zero
+	$display("1-HOT Encoding... %b",oZ);
+	$display("[%3g] Start test for %g-%g decoder...",$time, MYSIZE, OPSIZE);
+	for (loop=0;loop<OPSIZE;loop=loop+1)
+	begin
+		iS = loop;
+		#10;
+		$write("[%3g] Input=%b, Output=%b => ",$time, iS, oY);
+		if (oY != oZ) $display("[ERROR] Expected Output=%b",oZ);
+		else $display("[OK]");
+		oZ = oZ << 1;
+	end
+	$display("[%3g] End test.",$time);
+	// test 1-cold
+	oZ = 1; // only LSB 1, the rest should be zero
+	$display("1-COLD Encoding... %b",~oZ);
+	$display("[%3g] Start test for %g-%g decoder...",$time, MYSIZE, OPSIZE);
+	for (loop=0;loop<OPSIZE;loop=loop+1)
+	begin
+		iS = loop;
+		#10;
+		$write("[%3g] Input=%b, Output=%b => ",$time, iS, oX);
+		if (oX != ~oZ) $display("[ERROR] Expected Output=%b",~oZ);
+		else $display("[OK]");
+		oZ = oZ << 1;
+	end
+	$display("[%3g] End test.",$time);
+	$finish;
+end
+
+defparam dut.SEL_SIZE = MYSIZE;
+decoder dut ( iS, oY);
+defparam d2t.SEL_SIZE = MYSIZE;
+defparam d2t.ONE_COLD = 1;
+decoder d2t ( iS, oX);
+
+endmodule
