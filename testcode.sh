5eebcf10daf292a2b727adc5c3c87506fecb624a
Azman M. Yusof <azman@my1matrix.net>
2016-06-12 12:40:04 +0800

    Updated build/test script...
    - can now specify component (tb not run)

--- testcode.sh
+++ testcode.sh
@@ -12,16 +12,11 @@ VSIMPATH="$(which $VLOGEXEC 2>/dev/null)"
 VSIMWORK="work"
 VSIM_LOG="vlog.log"
 VSIM_OPT="-c -keepstdout -l $VSIM_LOG"
-SEL_CODE="$1"
 ONE_TIME="NO"
-KEEP_LIB="NO"
-KEEP_LOG="NO"
-
-# check if requested override for library cleanup
-[ "$(echo $@|grep -- '--keep-lib')" != "" ] && KEEP_LIB="YES"
-
-# check if requested override for log deletion
-[ "$(echo $@|grep -- '--keep-log')" != "" ] && KEEP_LOG="YES"
+DO_CLEAN="NO"
+SKIPTEST="NO"
+LIB_COMP="alu_add1b alu_add8b alu_sub1b alu_sub8b alu_logic alu_alu"
+LIB_COMP="${LIB_COMP} decoder zbuffer register registerfile"
 
 # make sure target path is available
 [ "$VSRCPATH" == "" ] && VSRCPATH="$(pwd)"
@@ -34,41 +29,83 @@ VSRCPATH=$(cd $VSRCPATH ; pwd)
 	echo "Cannot find verilog compiler '$VLOGEXEC'! Abort!" && exit 1
 VSIMPATH=$(dirname $VSIMPATH)
 
-# look for source files...
-codes=$(find ${VSRCPATH} -maxdepth 1 -name "*.v"|sort)
-[ "$codes" == "" ] && echo "No source file found!" && exit 0
+# look for available source files...
+lcode=$(find ${VSRCPATH} -maxdepth 1 -name "*.v"|sort)
+[ "$lcode" == "" ] && echo "No source file found!" && exit 0
+codes=""
+count=0
+comps=""
+
+# process params
+while [ "$1" != "" ]; do
+	case "$1" in
+		--clean) DO_CLEAN="YES" ;;
+		--compile) SKIPTEST="YES" ;;
+		--use-lib) # default component
+			for check in $LIB_COMP ; do
+				check=${VSRCPATH}/$check.v
+				[ -f $check ] && comps="$comps $check"
+			done
+			;;
+		--component)
+			shift
+			check=$(echo $lcode|grep "$1")
+			if [ "$check" != "" ] ; then
+				check=${VSRCPATH}/$1.v
+				[ -f $check ] && comps="$comps $check"
+			fi
+			;;
+		--*) ;; # ignore and continue
+		-*) echo "Invalid option? Aborting!" && exit 1 ;;
+		*)
+			# any particular module requested?
+			check=$(echo $lcode|grep "$1")
+			if [ "$check" != "" ] ; then
+				check=${VSRCPATH}/$1.v
+				[ -f $check ] && codes="$codes $check" && ((count++))
+			fi
+			;;
+	esac
+	shift
+done
+
+# if no module file specified, terminate?
+[ $count -eq 0 ] && echo "No module specified? Aborting!" && exit 1
+
+# check one-file option?
+[ $count -eq 1 ] && ONE_TIME="YES"
 
 # make sure compiler work path is ready
-echo -n "Creating work path for compiler... "
+echo -n "Check/create work path for compiler... "
 ${VSIMPATH}/${VLIBEXEC} ${VSIMWORK} >$VSIM_LOG
 [ $? -ne 0 ] && echo "ERROR! [$?]" && exit 1
 echo "done!"
 
-# any particular module requested?
-CHK_CODE=$(echo $codes|grep "$SEL_CODE")
-if [ "$CHK_CODE" != "" ] ; then
-	SEL_CODE=${VSRCPATH}/${SEL_CODE}.v
-	[ -f $SEL_CODE ] && codes=$SEL_CODE && ONE_TIME="YES"
-fi
-
-# do your thing...
-for code in $codes ; do
-	# strip file
-	a=$(basename $code)
-	# ignore testbenches
-	[ "${a//tb.v/}" != "$a" ] && continue
-	# display file and module name
-	echo -n "File: '$a' => Module: "
-	count=$(cat $code|grep -e '^[[:space:]]*module')
-	check=$(echo $count|sed -e 's/^.*module\s*\(\S*\)\s*(.*$/\1/')
+function get_module_name()
+{
+	local code="$1"
+	local count=$(cat $code|grep -e '^[[:space:]]*module')
+	local check=$(echo $count|sed -e 's/^.*module\s*\(\S*\)\s*(.*$/\1/')
 	count=$(echo $count|sed -e 's/^.*module.*(\(.*\)).*$/\1/'|tr -cd ,|wc -c)
 	((count++))
-	echo "'$check' {Param=$count}"
+	echo "$check:$count"
+}
+
+function do_compile()
+{
+	local what=1
+	local code="$1"
+	local type="design"
+	[ "${code//_tb.v/}" != "${code}" ] && type="testbench"
 	# try to compile
-	echo -n "    Checking design for sim-ready... "
+	echo -n "    Checking $type for sim-ready... "
 	${VSIMPATH}/${VLOGEXEC} $code >$VSIM_LOG
 	if [ $? -eq 0 ] ; then
 		echo "done!"
+		what=0
+		# remove module per request
+		[ "$(echo $@|grep -- '--remove-lib')" != "" ] &&
+			${VSIMPATH}/${VDELEXEC} ${info[0]}
 	else
 		echo "compile error!"
 		read -n 1 -p "[S]how error or press any key to continue..." dummy
@@ -78,40 +115,18 @@ for code in $codes ; do
 			read -n 1 -p "Press any key to continue..." dummy
 			echo
 		fi
-		continue
 	fi
-	# look for testbench
-	code_tb=${code//.v/_tb.v}
-	if [ ! -f "$code_tb" ] ; then
-		echo "  Cannot find test bench for '$a'!"
-		read -n 1 -p "Press any key to continue..." dummy
-		echo
-		${VSIMPATH}/${VDELEXEC} $check
-		continue
-	fi
-	# compile test bench
-	echo -n "    Checking testbench for sim-ready... "
-	${VSIMPATH}/${VLOGEXEC} $code_tb >>$VSIM_LOG
-	if [ $? -eq 0 ] ; then
-		echo "done!"
-	else
-		echo "compile error!" ; echo
-		read -n 1 -p "[S]how error or press any key to continue..." dummy
-		echo
-		if [ "$dummy" == "S" -o "$dummy" == "s" ] ; then
-			echo "$(cat $VSIM_LOG|grep -e Error -e Warning)"
-			read -n 1 -p "Press any key to continue..." dummy
-			echo
-		fi
-		exit 1
-	fi
-	# find test bench module
-	bench=$(cat $code_tb|grep -e '^[[:space:]]*module')
-	bench=$(echo $bench|sed -e 's/^.*module\s*\(\S*\)\s*(.*$/\1/')
-	echo "    Test Module: '$bench'"
+	# remove log
+	rm -rf $VSIM_LOG
+	# return status
+	return $what
+}
+
+function do_testing()
+{
 	# run simulation
 	echo -n "    Running testbench simulation... "
-	${VSIMPATH}/${VSIMEXEC} ${VSIM_OPT} work.$bench -do "run -all" &>$VSIM_LOG
+	${VSIMPATH}/${VSIMEXEC} ${VSIM_OPT} work.$1 -do "run -all" &>$VSIM_LOG
 	VSIM_RES=$?
 	echo "completed! [$VSIM_RES]"
 	if [ $VSIM_RES -eq 0 ] ; then
@@ -119,23 +134,66 @@ for code in $codes ; do
 	else
 		echo "$(cat $VSIM_LOG)"
 	fi
-	echo "    Done checking module '$check'."
+	# remove log
+	rm -rf $VSIM_LOG
+}
+
+# build components?
+for comp in $comps ; do
+	file=$(basename $comp)
+	info=$(get_module_name $comp)
+	name=${info%:*}
+	info=${info#*:}
+	# display file and module name
+	echo "File: '$file' => Module: '$name' {Param=$info}"
+	# compile design
+	do_compile $comp
+done
+
+# do your thing...
+for code in $codes ; do
+	# ignore testbench
+	[ "${code//_tb.v/}" != "${code}" ] && continue
+	file=$(basename $code)
+	info=$(get_module_name $code)
+	name=${info%:*}
+	info=${info#*:}
+	# display file and module name
+	echo "File: '$file' => Module: '$name' {Param=$info}"
+	# compile design
+	do_compile $code
+	[ $? -ne 0 ] && continue
+	# compile only?
+	[ "$SKIPTEST" == "YES" ] && continue
+	# look for testbench
+	code_tb=${code//.v/_tb.v}
+	if [ ! -f "$code_tb" ] ; then
+		echo "  Cannot find testbench for '$file'!"
+		read -n 1 -p "Press any key to continue..." dummy
+		echo
+		continue
+	fi
+	info=$(get_module_name $code_tb)
+	name_tb=${info%:*}
+	# display file and module name
+	echo "File: '$code_tb' => Module: '$name_tb'"
+	# compile testbench
+	do_compile $code_tb
+	[ $? -ne 0 ] && continue
+	# simulate testbench
+	do_testing $name_tb
+	# post check
+	echo "    Done checking module '$name'."
 	if [ "$ONE_TIME" != "YES" ] ; then
 		read -n 1 -p "Press any key to continue..." dummy
 		echo
 	fi
-	if [ "$KEEP_LIB" == "NO" ] ; then
-		# removes compiled library - in case same name with the next
-		${VSIMPATH}/${VDELEXEC} $check
-		${VSIMPATH}/${VDELEXEC} $bench
-	fi
-	# remove log
-	[ "$KEEP_LOG" == "NO" ] && rm -rf $VSIM_LOG
 done
 
-if [ "$KEEP_LIB" == "NO" ] ; then
+if [ "$DO_CLEAN" == "YES" ] ; then
 	# clean up compiler work path
 	echo -n "Removing work path for compiler... "
-	[ -d $VSIMWORK ] && rm -rf $VSIMWORK
+	${VSIMPATH}/${VDELEXEC} -all
+	#[ -d $VSIMWORK ] && rm -rf $VSIMWORK
 	echo "done!"
 fi
