3227cfc1c82f9f8a58d1e050d103a8c3ae8bc575
Azman M. Yusof <azman@my1matrix.net>
2016-06-16 07:00:12 +0800

    Working on core_control
    - need to workout a few things...

--- vlog/core_control.v
+++ vlog/core_control.v
@@ -1,192 +1,310 @@
-module control (clk, rst, code, ipin, opin);
+module control (clk_, rst_, inst, ipin, oenb, opin);
 
-parameter DATASIZE = 8;
-parameter STATE_TR = 4'b0000; // reset state
-parameter STATE_T1 = 4'b0001; // state 1
-parameter STATE_T2 = 4'b0010; // state 2
-parameter STATE_T3 = 4'b0011; // state 3
-parameter STATE_T4 = 4'b0100; // state 4
-parameter STATE_T5 = 4'b0101; // state 5
-parameter STATE_T6 = 4'b0110; // state 6
-parameter STATE_TH = 4'b0111; // hold state
-parameter STATE_TW = 4'b1000; // wait state
-parameter STATE_TT = 4'b1001; // halt state
-parameter STAT_OF = 3'b011; // opcode fetch
-parameter STAT_MW = 3'b001; // memory write
-parameter STAT_MR = 3'b010; // memory read
-parameter STAT_DW = 3'b101; // device write
-parameter STAT_DR = 3'b110; // device read
-parameter STAT_INTA = 3'b111; // interrupt acknowledge
-parameter STAT_HALT = 3'bz00; // halt
-parameter STAT_HRST = 3'bzxx; // hold/reset
+// state register
+parameter STATECNT = 10;
+parameter STATE_TR = 10'b0000000001; // reset state
+parameter STATE_T1 = 10'b0000000010; // state 1
+parameter STATE_T2 = 10'b0000000100; // state 2
+parameter STATE_T3 = 10'b0000001000; // state 3
+parameter STATE_T4 = 10'b0000010000; // state 4
+parameter STATE_T5 = 10'b0000100000; // state 5
+parameter STATE_T6 = 10'b0001000000; // state 6
+parameter STATE_TH = 10'b0010000000; // hold state
+parameter STATE_TW = 10'b0100000000; // wait state
+parameter STATE_TT = 10'b1000000000; // halt state
+// machine cycle status & control - ctrl:{inta_,wr_,rd_}, stat:{io/m_,s1,s0}
+parameter CYCLE_OF = 6'b110011; // opcode fetch
+parameter CYCLE_MW = 6'b101001; // memory write
+parameter CYCLE_MR = 6'b110010; // memory read
+parameter CYCLE_DW = 6'b101101; // device write
+parameter CYCLE_DR = 6'b110110; // device read
+parameter CYCLE_INA = 6'b011111; // interrupt acknowledge
+parameter CYCLE_BID = 6'b111010; // bus idle - DAD (2 cycles?)
+parameter CYCLE_BIT = 6'b111111; // ack reset/trap
+parameter CYCLE_BIH = 6'b111100; // halt ; actually 6'b1zzz00
+parameter STAT_S0 = 0;
+parameter STAT_S1 = 1;
+parameter STAT_IOM_ = 2;
+parameter CTRL_RD_ = 3;
+parameter CTRL_WR_ = 4;
+parameter CTRL_INTA_ = 5;
+// these are from alureg
+parameter INST_GO6 = 0;
+parameter INST_DAD = 1;
+parameter INST_HLT = 2;
+parameter INSTSIZE = 3;
+// input pins
 parameter IPIN_READY = 0;
 parameter IPIN_HOLD = 1;
 parameter IPIN_COUNT = 2;
+// output enables (control)
+parameter OENB_ADDL = 0;
+parameter OENB_ADDH = 1;
+parameter OENB_DATA = 2;
+parameter OENB_COUNT = 3;
+// direct ouput pins
 parameter OPIN_S0 = 0;
 parameter OPIN_S1 = 1;
 parameter OPIN_IOM_ = 2;
-parameter OPIN_HLDA = 3;
-parameter OPIN_RD_ = 4;
-parameter OPIN_WR_ = 5;
-parameter OPIN_INTA_ = 6;
-parameter OPIN_ALE = 7;
-parameter OPIN_COUNT = 8;
+parameter OPIN_RD_ = 3;
+parameter OPIN_WR_ = 4;
+parameter OPIN_INTA_ = 5;
+parameter OPIN_ALE = 6;
+parameter OPIN_COUNT = 7;
 
-input clk,rst;
-input[DATASIZE-1:0] code; // code from Instruction Register
+// port i/o declaration
+input clk_,rst_;
+input[INSTSIZE-1:0] inst;
 input[IPIN_COUNT-1:0] ipin;
+output[OENB_COUNT-1:0] oenb;
 output[OPIN_COUNT-1:0] opin;
+wire[OENB_COUNT-1:0] oenb;
 wire[OPIN_COUNT-1:0] opin;
 
-// internal registers & nodes
-reg[3:0] cstate; // 4-bit encoded states = 6+4 used states
-reg[2:0] stat, ctrl; // stat:{io/m_,s1,s0} , ctrl:{inta_,wr_,rd_}
-reg hlda, ale, bimc, halt, lmc, fmc, go6, putZ;
-reg[3:0] nstate; // encoded calculated next state values
+// internal registers
+reg[STATECNT-1:0] cstate, nstate; // 1-hot encoded states
+reg[6:0] stactl; // stat:{io/m_,s1,s0} , ctrl:{inta_,wr_,rd_}
+reg pin_ale, pin_ia_, pin_wr_, pin_rd_, pin_im_, pin_sta;
+reg enb_adh, enb_adl, enb_dat, enb_ctl;
+// status registers
+reg is_bimc, is_last, is_init;
 
+// drive enb port with internal registers
+assign oenb[OENB_ADDL] = enb_adl;
+assign oenb[OENB_ADDH] = enb_adh;
+assign oenb[OENB_DATA] = enb_dat;
 // direct reg to pin
-assign opin[OPIN_S0] = stat[0];
-assign opin[OPIN_S1] = stat[1];
-assign opin[OPIN_IOM_] = putZ ? 1'bz : stat[2];
-assign opin[OPIN_HLDA] = hlda;
-assign opin[OPIN_INTA_] = ctrl[2];
-assign opin[OPIN_WR_] = putZ ? 1'bz : ctrl[1];
-assign opin[OPIN_RD_] = putZ ? 1'bz : ctrl[2];
+assign opin[OPIN_S0] = pin_sta | stactl[STAT_S0];
+assign opin[OPIN_S1] = pin_sta | stactl[STAT_S1];
+assign opin[OPIN_IOM_] = enb_ctl ? 1'bz : pin_im_ & stactl[STAT_IOM_];
+assign opin[OPIN_RD_] = enb_ctl ? 1'bz : pin_rd_ | stactl[CTRL_RD_];
+assign opin[OPIN_WR_] = enb_ctl ? 1'bz : pin_wr_ | stactl[CTRL_WR_];
+assign opin[OPIN_INTA_] = pin_ia_ | stactl[CTRL_INTA_];
+assign opin[OPIN_ALE] = pin_ale;
 
-// state machine trigger
-always @(posedge clk or posedge rst)  // asynchronous reset!?
-begin
-	if(rst == 1) begin // actually active low
-		// should remain low for 10ms after min vcc
-		// 3 clock cycles for correct reset operation?
-		cstate <= STATE_TR;
-		// internal register & node updates??
-		stat <= STAT_OF;
-		ctrl <= 3'b111; // inta_:hi, wr_:hi, rd_:hi
-		hlda <= 1'b0;
-		ale <= 1'b0;
-		bimc <= 1'b0; // not bus idle cycle
-		halt <= 1'b0; // no halt
-		lmc <= 1'b0; // not last cycle
-		fmc <= 1'b1; // is first cycle
-		go6 <= 1'b0; // not 6-state cycle
-		putZ <= 1'b1; // high when in reset/hold/halt states
-	end
-	else begin
-		cstate <= nstate;
+// output logic
+always @(posedge clk_) begin
+	if(rst_ == 1) begin // actually active low
+		stactl <= CYCLE_OF;
+		pin_ale <= 1'b0;
+		pin_ia_ <= 1'b1;
+		pin_wr_ <= 1'b0;
+		pin_rd_ <= 1'b0;
+		pin_im_ <= 1'b1;
+		pin_sta <= 1'b0;
+		enb_adh <= 1'b0;
+		enb_adl <= 1'b0;
+		enb_dat <= 1'b0;
+		enb_ctl <= 1'b0;
+		is_bimc <= 1'b0;
+		is_last <= 1'b0;
+		is_init <= 1'b1;
+	end else begin
 		case (cstate)
-			STATE_T2:
-				hlda <= 1'b0;
-		endcase
-		case (nstate)
-			STATE_TR: begin
-				stat <= STAT_OF;
-				ctrl <= 3'b111; // inta_:hi, wr_:hi, rd_:hi
-				hlda <= 1'b0;
-				ale <= 1'b0;
-				bimc <= 1'b0; // not bus idle cycle
-				halt <= 1'b0; // no halt
-				lmc <= 1'b0; // not last cycle
-				fmc <= 1'b1; // is first cycle
-				go6 <= 1'b0; // not 6-state cycle
-				putZ <= 1'b1; // high when in reset/hold/halt states
-			end
 			STATE_T1: begin
-				hlda <= 1'b0;
-				ale <= 1'b1; // except 2nd/3rd cycle of DAD
+				if (is_bimc|inst[INST_DAD]) //inst[INST_DAD] always bimc?
+					pin_ale <= 1'b0;
+				else
+					pin_ale <= 1'b1;
+				pin_ia_ <= 1'b1; // must be high
+				pin_wr_ <= 1'b1; // must be high
+				pin_rd_ <= 1'b1; // must be high
+				pin_im_ <= 1'b1;
+				pin_sta <= 1'b0;
+				enb_adh <= 1'b1; // always enable T1-T6
+				enb_adl <= 1'b1;
+				enb_dat <= 1'b0;
+				enb_ctl <= 1'b1;
 			end
 			STATE_T2: begin
-				hlda <= 1'b0;
-			end
-			STATE_T3: begin
-				hlda <= 1'b0;
-			end
-			STATE_T4: begin
-				hlda <= 1'b0;
-			end
-			STATE_T5: begin
-				hlda <= 1'b0;
-			end
-			STATE_T6: begin
-				hlda <= 1'b0;
+				pin_ale <= 1'b0;
+				pin_ia_ <= 1'b0; // depends on machine cycle
+				pin_wr_ <= 1'b0; // depends on machine cycle
+				pin_rd_ <= 1'b0; // depends on machine cycle
+				pin_im_ <= 1'b1;
+				pin_sta <= 1'b0;
+				enb_adh <= 1'b1; // always enable T1-T6
+				enb_adl <= 1'b0;
+				enb_dat <= ~stactl[CTRL_WR_]; // enable only if writing
+				enb_ctl <= 1'b1;
 			end
 			STATE_TW: begin
-				hlda <= 1'b0;
+				pin_ale <= 1'b0;
+				pin_ia_ <= 1'b0; // depends on machine cycle
+				pin_wr_ <= 1'b0; // depends on machine cycle
+				pin_rd_ <= 1'b0; // depends on machine cycle
+				pin_im_ <= 1'b1;
+				pin_sta <= 1'b0;
+				enb_adh <= 1'b1; // always enable T1-T6
+				enb_adl <= 1'b0;
+				enb_dat <= ~stactl[CTRL_WR_]; // enable only if writing
+				enb_ctl <= 1'b1;
 			end
-			STATE_TH: begin
-				hlda <= 1'b1;
+			STATE_T3: begin
+				pin_ale <= 1'b0;
+				pin_ia_ <= 1'b0; // depends on machine cycle
+				pin_wr_ <= 1'b0; // depends on machine cycle
+				pin_rd_ <= 1'b0; // depends on machine cycle
+				pin_im_ <= 1'b1;
+				pin_sta <= 1'b0;
+				enb_adh <= 1'b1; // always enable T1-T6
+				enb_adl <= 1'b0;
+				enb_dat <= ~stactl[CTRL_WR_]; // enable only if writing
+				enb_ctl <= 1'b1;
+			end
+			STATE_T4: begin
+				pin_ale <= 1'b0;
+				pin_ia_ <= 1'b1; // must be high
+				pin_wr_ <= 1'b1; // must be high
+				pin_rd_ <= 1'b1; // must be high
+				pin_im_ <= 1'b0; // overrides status lines
+				pin_sta <= 1'b1; // overrides status lines
+				enb_adh <= 1'b1; // always enable T1-T6
+				enb_adl <= 1'b0; // high-z T4-T6
+				enb_dat <= 1'b0; // high-z T4-T6
+				enb_ctl <= 1'b1;
+				// check next machine cycle here?
+				is_bimc <= 1'b0;
+				is_last <= 1'b0;
+				is_init <= 1'b1;
+			end
+			STATE_T5: begin
+				pin_ale <= 1'b0;
+				pin_ia_ <= 1'b1; // must be high
+				pin_wr_ <= 1'b1; // must be high
+				pin_rd_ <= 1'b1; // must be high
+				pin_im_ <= 1'b0; // overrides status lines
+				pin_sta <= 1'b1; // overrides status lines
+				enb_adh <= 1'b1; // always enable T1-T6
+				enb_adl <= 1'b0; // high-z T4-T6
+				enb_dat <= 1'b0; // high-z T4-T6
+				enb_ctl <= 1'b1;
+			end
+			STATE_T6: begin
+				pin_ale <= 1'b0;
+				pin_ia_ <= 1'b1; // must be high
+				pin_wr_ <= 1'b1; // must be high
+				pin_rd_ <= 1'b1; // must be high
+				pin_im_ <= 1'b0; // overrides status lines
+				pin_sta <= 1'b1; // overrides status lines
+				enb_adh <= 1'b1; // always enable T1-T6
+				enb_adl <= 1'b0; // high-z T4-T6
+				enb_dat <= 1'b0; // high-z T4-T6
+				enb_ctl <= 1'b1;
+			end
+			STATE_TR: begin
+				stactl <= CYCLE_OF; // always reset to opcode fetch cycle
+				pin_ale <= 1'b0;
+				pin_ia_ <= 1'b1;
+				pin_wr_ <= 1'b0;
+				pin_rd_ <= 1'b0;
+				pin_im_ <= 1'b1;
+				pin_sta <= 1'b0;
+				enb_adh <= 1'b0;
+				enb_adl <= 1'b0;
+				enb_dat <= 1'b0;
+				enb_ctl <= 1'b0;
+				is_bimc <= 1'b0;
+				is_last <= 1'b0;
+				is_init <= 1'b1;
 			end
 			STATE_TT: begin
-				hlda <= 1'b0;
+				pin_ale <= 1'b0;
+				pin_ia_ <= 1'b1;
+				pin_wr_ <= 1'b0;
+				pin_rd_ <= 1'b0;
+				pin_im_ <= 1'b1;
+				pin_sta <= 1'b0;
+				enb_adh <= 1'b0;
+				enb_adl <= 1'b0;
+				enb_dat <= 1'b0;
+				enb_ctl <= 1'b0;
+			end
+			STATE_TH: begin
+				pin_ale <= 1'b0;
+				pin_ia_ <= 1'b1;
+				pin_wr_ <= 1'b0;
+				pin_rd_ <= 1'b0;
+				pin_im_ <= 1'b1;
+				pin_sta <= 1'b0;
+				enb_adh <= 1'b0;
+				enb_adl <= 1'b0;
+				enb_dat <= 1'b0;
+				enb_ctl <= 1'b0;
 			end
 		endcase
 	end
 end
 
-// state selection logic
-always @(cstate or ipin or stat or bimc or halt or lmc or fmc or go6)
-begin
+// next-state logic
+always @(cstate or inst or ipin or stactl or
+		is_bimc or is_last or is_init) begin
+	nstate = cstate;
 	case (cstate)
 		STATE_TR: begin
-			nstate <= STATE_T1;
+			nstate = STATE_T1;
 		end
 		STATE_T1: begin
-			if (halt) begin
-				nstate <= STATE_TT;
-			end
-			else begin
-				nstate <= STATE_T2;
+			if (inst[INST_HLT]) begin
+				nstate = STATE_TT;
+			end else begin
+				nstate = STATE_T2;
 			end
 		end
 		STATE_T2: begin
-			if (ipin[IPIN_READY]|bimc) begin
-				nstate <= STATE_T3;
-			end
-			else begin
-				nstate <= STATE_TW;
+			if (ipin[IPIN_READY]|is_bimc) begin
+				nstate = STATE_T3;
+			end else begin
+				nstate = STATE_TW;
 			end
 		end
 		STATE_T3: begin
-			if (fmc) begin
-				nstate <= STATE_T4;
-			end
-			else begin
-				nstate <= STATE_T1;
+			if (is_init) begin
+				nstate = STATE_T4;
+			end else begin
+				nstate = STATE_T1;
 			end
 		end
 		STATE_T4: begin
-			if (go6) begin
-				nstate <= STATE_T5;
-			end
-			else begin
-				nstate <= STATE_T1;
+			if (inst[INST_GO6]) begin
+				nstate = STATE_T5;
+			end else begin
+				nstate = STATE_T1;
 			end
 		end
 		STATE_T5: begin
-			nstate <= STATE_T6;
+			nstate = STATE_T6;
 		end
 		STATE_T6: begin
-			nstate <= STATE_T1;
+			nstate = STATE_T1;
 		end
 		STATE_TW: begin
-			if (ipin[IPIN_READY]|bimc) begin
-				nstate <= STATE_T3;
+			if (ipin[IPIN_READY]|is_bimc) begin
+				nstate = STATE_T3;
 			end
 		end
 		STATE_TH: begin
 			if (~ipin[IPIN_HOLD]) begin
-				if (halt) nstate <= STATE_TT;
-				else nstate <= STATE_T1;
+				if (inst[INST_HLT]) nstate = STATE_TT;
+				else nstate = STATE_T1;
 			end
 		end
 		STATE_TT: begin
 			if (ipin[IPIN_HOLD]) begin
-				nstate <= STATE_TH;
+				nstate = STATE_TH;
 			end
+			// valid interrupt can get us out back to circulation?
 		end
 	endcase
 end
 
-// output assignment
+// state register - transition on negative edge!
+always @(posedge clk_ or posedge rst_) begin // asynchronous reset, active low
+	if(rst_ == 1) begin // actually active low
+		cstate <= STATE_TR;
+	end else begin
+		cstate <= nstate;
+	end
+end
 
 endmodule
--- vlog/core_control_tb.v
+++ vlog/core_control_tb.v
@@ -1,38 +1,39 @@
 module core_control_tb();
 
-parameter DATASIZE = 8;
 parameter CLKPTIME = 10;
 parameter IPIN_READY = 0;
 parameter IPIN_HOLD = 1;
-parameter IPIN_COUNT = 2;
-parameter OPIN_COUNT = 8;
+parameter IPIN_COUNT = dut.IPIN_COUNT;
+parameter OENB_COUNT = dut.OENB_COUNT;
+parameter OPIN_COUNT = dut.OPIN_COUNT;
 
 reg clk,rst;
-reg[DATASIZE-1:0] code;
+reg[dut.INSTSIZE-1:0] inst; // decoded instruction info
 reg[IPIN_COUNT-1:0] ipin;
+wire[OENB_COUNT-1:0] oenb;
 wire[OPIN_COUNT-1:0] opin;
 
-	// reset block
-	initial begin
-		clk = 1'b0; rst = 1'b1; code = {DATASIZE{1'b0}};
-		ipin = {IPIN_COUNT{1'b0}};
-		ipin[IPIN_READY] = 1'b1; // memory always ready!
-		$monitor("[%04g] STATE: %b {%b,%b,%b}",$time/CLKPTIME,dut.cstate,
-			code,ipin,opin);
-		#(CLKPTIME*3) rst = 1'b0;
-	end
+// reset block
+initial begin
+	clk = 1'b1; rst = 1'b0; inst = {dut.INSTSIZE{1'b0}};
+	ipin = {IPIN_COUNT{1'b0}};
+	ipin[IPIN_READY] = 1'b1; // memory always ready!
+	$monitor("[%04g] STATE: %b {%b,%b,%b,%b}",$time/CLKPTIME,
+		dut.cstate,inst,ipin,oenb,opin);
+	#(CLKPTIME*3) rst = 1'b1;
+end
 
-	// generate clock
-	always begin
-		#(CLKPTIME/2) clk = !clk;
-	end
+// generate clock
+always begin
+	#(CLKPTIME/2) clk = !clk;
+end
 
-	//generate stimuli
-	always begin
-		// just monitoring state - not feeding anything
-		#(CLKPTIME*50); $finish;
-	end
+//generate stimuli
+always begin
+	// just monitoring state - not feeding anything
+	#(CLKPTIME*50); $finish;
+end
 
-	control dut (clk, rst, code, ipin, opin);
+control dut (~clk, ~rst, inst, ipin, oenb, opin);
 
 endmodule
